326f315ff97520011c7f9b9f7d2042dea21628d8
midn1ght7 <midn1ght7@users.noreply.github.com>
2022-05-01 16:47:17 +0200

    Fix + Previews

--- learning.py
+++ learning.py
@@ -84,9 +84,11 @@ net = tflearn.fully_connected(net, len(output[0]), activation="softmax") #daje p
 net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
-
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
     
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
